ea948b3d5ee34f965c0a90e3291fb95e9ad75b97
AjayShelar <ajaysanjayshelar@Ajays-MacBook-Air.local>
2018-10-28 16:50:22 +0530

    fix settings

--- zaya/zaya/settings.py
+++ zaya/zaya/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+SITE_ID = 1
 
 ROOT_URLCONF = 'zaya.urls'
 
